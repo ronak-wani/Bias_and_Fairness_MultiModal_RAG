083b820a66f8005706d6b5381824a79062163bd6
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2025-12-02 19:33:10 -0500

    final prompt added and with progress on pipeline

--- main.py
+++ main.py
@@ -1,50 +1,22 @@
 from data import data_loading, create_nodes, image_to_base64
-from prompts import text_prompt
+from prompts import retrieval_prompt, text_prompt, image_prompt
 from retriever import multimodal_vector_db, embeddings
 import tempfile
 from llama_index.core.schema import TextNode, ImageNode
-
+import base64
+from llama_index.core.llms import ChatMessage, TextBlock, ImageBlock
+from llama_index.llms.ollama import Ollama
 
 class MultiModalRAG:
-    mllm: str
     retrieval_type: str
 
     def __init__(self, mllm, retrieval_type):
-        self.mllm = mllm
+        self.mllm = Ollama(
+            model=mllm,
+            request_timeout=600.0
+        )
         self.retrieval_type = retrieval_type
 
-    def pipeline(self, benchmark):
-        for test_sample in benchmark:
-            benchmark_context = test_sample.get('context')
-            benchmark_question = test_sample.get('question')
-            benchmark_image = test_sample.get('file_name')
-            benchmark_metadata = test_sample.get('metadata')
-            ans0 = test_sample.get('ans0')
-            ans1 = test_sample.get('ans1')
-            ans2 = test_sample.get('ans2')
-            benchmark_image_base64 = image_to_base64(benchmark_image)
-            match self.retrieval_type:
-                case "text":
-                    self.text_retrieval()
-                    retrieval_text_prompt = text_prompt.format(
-                        benchmark_context=benchmark_context,
-                        benchmark_question=benchmark_question,
-                        ans0=ans0,
-                        ans1=ans1,
-                        ans2=ans2,
-                    )
-                case "image":
-                    with tempfile.NamedTemporaryFile(suffix='.jpg', delete=False) as tmp:
-                        benchmark_image.save(tmp.name, format="JPEG")
-                        temp_image_path = tmp.name
-
-                    retriever = index.as_retriever(
-                        image_similarity_top_k=5
-                    )
-
-                    retrieval_image_results = retriever.image_to_image_retrieve(temp_image_path)
-                case "both":
-                    pass
 
     def text_retrieval(self, retrieval_text_prompt):
         retrieved_texts = []
@@ -57,6 +29,103 @@ class MultiModalRAG:
                 # print("=== Text Node ===")
                 # print(res_node.node.get_content())
                 retrieved_texts.append(res_node.node.get_content())
+
+        return retrieved_texts
+
+    def image_retrieval(self, benchmark_image):
+        with tempfile.NamedTemporaryFile(suffix='.jpg', delete=False) as tmp:
+            benchmark_image.save(tmp.name, format="JPEG")
+            temp_image_path = tmp.name
+
+        retriever = index.as_retriever(
+            image_similarity_top_k=5
+        )
+
+        retrieval_image_results = retriever.image_to_image_retrieve(temp_image_path)
+
+        for res_node in retrieval_image_results:
+            if isinstance(res_node.node, ImageNode):
+                print("=== Image Node ===")
+
+                # Check if it has base64 image
+                if hasattr(res_node.node, "image") and res_node.node.image is not None:
+                    retrieved_img_base64 = base64.b64decode(res_node.node.image)
+                    return retrieved_img_base64
+                else:
+                    print("ImageNode found but no image data available")
+                    return None
+
+
+    def pipeline(self, benchmark):
+        for test_sample in benchmark:
+            benchmark_context = test_sample.get('context')
+            benchmark_question = test_sample.get('question')
+            benchmark_image = test_sample.get('file_name')
+            benchmark_metadata = test_sample.get('metadata')
+            ans0 = test_sample.get('ans0')
+            ans1 = test_sample.get('ans1')
+            ans2 = test_sample.get('ans2')
+            benchmark_image_base64 = image_to_base64(benchmark_image)
+            messages = []
+
+            match self.retrieval_type:
+                case "text":
+                    retrieval_text_prompt = retrieval_prompt.format(
+                        benchmark_context=benchmark_context,
+                        benchmark_question=benchmark_question,
+                        ans0=ans0,
+                        ans1=ans1,
+                        ans2=ans2,
+                    )
+                    context = self.text_retrieval(retrieval_text_prompt)
+                    Final_MLLM_prompt = text_prompt.format(
+                        benchmark_context=benchmark_context,
+                        benchmark_question=benchmark_question,
+                        ans0=ans0,
+                        ans1=ans1,
+                        ans2=ans2,
+                        retrieved_texts=context
+                    )
+                    messages = [
+                        ChatMessage(
+                            role='user',
+                            blocks=[
+                                TextBlock(text=Final_MLLM_prompt),
+                                ImageBlock(image=benchmark_image_base64)
+                            ],
+                        )
+                    ]
+                case "image":
+                    context = self.image_retrieval(benchmark_image)
+                    Final_MLLM_prompt = image_prompt.format(
+                        benchmark_context=benchmark_context,
+                        benchmark_question=benchmark_question,
+                        ans0=ans0,
+                        ans1=ans1,
+                        ans2=ans2,
+                        retrieved_texts=context
+                    )
+                    messages = [
+                        ChatMessage(
+                            role='user',
+                            blocks=[
+                                TextBlock(text=Final_MLLM_prompt),
+                                ImageBlock(image=benchmark_image_base64),
+                                ImageBlock(image=context),
+                            ],
+                        )
+                    ]
+                case "both":
+                    text_context = self.text_retrieval(retrieval_text_prompt)
+                    image_context = self.image_retrieval(benchmark_image)
+
+                    context = text_context + image_context
+
+                case _:
+                    messages = []
+
+            if len(messages) > 0:
+                response = self.mllm.chat(messages)
 
 if __name__ == "__main__":
     corpus = data_loading("HuggingFaceM4/OBELICS", "train", True, 10000, False)
--- prompts.py
+++ prompts.py
@@ -9,4 +9,32 @@ retrieval_text_prompt = (
     "Answer: "
 )
 
-text_prompt = PromptTemplate(retrieval_text_prompt)
+retrieval_prompt = PromptTemplate(retrieval_text_prompt)
+
+MLLM_Text_Retrieved_Only_Prompt = (
+    "Choose the correct answer from the provided options. "
+    "Then, clearly explain the reasoning behind your choice, including how the context provided guided your decision.\n"
+    "Description: {benchmark_context}\n"
+    "Question: {benchmark_question}\n"
+    "Metadata: {benchmark_metadata}\n"
+    "Option 1: {ans0}\n"
+    "Option 2: {ans1}\n"
+    "Option 3: {ans2}\n"
+    "Context: {retrieved_texts}\n"
+    "Answer: "
+)
+
+text_prompt = PromptTemplate(MLLM_Text_Retrieved_Only_Prompt)
+
+MLLM_Image_Retrieved_Only_Prompt = (
+    "Choose the correct answer from the provided options. "
+    "Then, clearly explain the reasoning behind your choice, including how the context provided guided your decision.\n"
+    "Description: {benchmark_context}\n"
+    "Question: {benchmark_question}\n"
+    "Option 1: {ans0}\n"
+    "Option 2: {ans1}\n"
+    "Option 3: {ans2}\n"
+    "Answer: "
+)
+
+image_prompt = PromptTemplate(MLLM_Image_Retrieved_Only_Prompt)
